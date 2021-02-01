6c4b343382bf313bfc6a4871f9b2f3cb99ca80d5
William Felipe <williamfelipe34@gmail.com>
2021-02-01 18:08:28 -0300

    api git hub

--- src/pages/Dashbord/index.tsx
+++ src/pages/Dashbord/index.tsx
@@ -1,32 +1,74 @@
-import React from 'react';
-import { FiChevronRight } from 'react-icons/fi'
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import logoImage from '../../asset/logo.svg'
+import api from '../../services/api';
 
-import { Title, Form, Repositories } from './style'
+import logoImage from '../../asset/logo.svg';
+
+import { Title, Form, Repositories, Error } from './style';
+
+interface Repositoy{ 
+  full_name: string,
+  description: string
+  owner : {
+    login: string,
+    avatar_url: string
+  }
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repostitories, setRepositories] = useState<Repositoy[]>([]);
+  const [ inputError, setInputError ] = useState('');
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+      event.preventDefault();
+      
+      if(!newRepo){
+        setInputError('Digite o nome do Repositório');
+        return;
+      }
+
+      try {
+        const response = await api.get<Repositoy>(`repos/${newRepo}`);
+
+        const repository = response.data;
+        setRepositories([...repostitories, repository]);
+        setNewRepo('');
+        setInputError('');
+      } catch (error) {
+        setInputError('Error na busca de repositório')
+      }
+     
+  }
+
   return (
     <>
-      <img src={logoImage} alt="Github Explorer"></img>
+      <img src={ logoImage } alt="Github Explorer"></img>
       <Title>Explore repositórios no GitHub</Title>
-      <Form>
-        <input placeholder="Digite o nome do repositório"/>
+      <Form hasError={ !!inputError } onSubmit={ handleAddRepository }>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit" >Pesquisar</button>
       </Form>
-
+      { inputError && <Error>{ inputError }</Error>}
       <Repositories>
-        <a href="teste">
-          <img 
-            src="https://avatars.githubusercontent.com/u/21224558?s=460&u=f6608225c4f9ca6b6b45471d5a78336304cec98d&v=4"
-            alt="William Felipe" />
-          <div>
-            <strong>william felipe</strong>
-            <p>TESTES DE DESCRIÇÂO</p>
-          </div>
+        {repostitories.map(repository => (
+          <a key={ repository.full_name } href="teste">
+            <img 
+              src={ repository.owner.avatar_url }
+              alt={ repository.owner.login } />
+            <div>
+              <strong>{ repository.full_name }</strong>
+              <p>{ repository.description }</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   ) 
